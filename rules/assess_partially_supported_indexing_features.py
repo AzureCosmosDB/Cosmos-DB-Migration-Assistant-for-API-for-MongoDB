0f1e88f162cbeb6053276a09545839ef9a4ba7e5
Shweta Nayak <16984456+nayakshweta@users.noreply.github.com>
2021-11-30 16:32:36 +0530

    Adds rule to check for unique indexes using nested fields.

--- rules/assess_partially_supported_indexing_features.py
+++ rules/assess_partially_supported_indexing_features.py
@@ -1,4 +1,6 @@
 from .assess_compound_index_nested_field import *
+from .assess_unique_index_nested_field import *
 
 def assess_partially_supported_indexing_features(self):
     assess_compound_index_nested_field(self)
+    assess_unique_index_nested_field(self)
---
+++ rules/assess_unique_index_nested_field.py
@@ -0,0 +1,20 @@
+def assess_unique_index_nested_field(self):
+    unique_index_nested_count = 0
+    for db in self.databases:
+        for col in db.collections:
+            for index in col.indexes.values():
+                # check if it is a unique index
+                if 'unique' in index.keys():
+                    # look for nested field in the index - works for single or compound unique index
+                    for i, index_condition in enumerate(index['key']):
+                        index_field = index_condition[0]
+                        if "." in index_field:
+                            print("Collection",col.collection_name,"in Database",db.database_name,"is using unique index with nested field.")
+                            unique_index_nested_count+=1
+                            break
+
+    if unique_index_nested_count!=0:
+        unique_index_nested_msg = "Unique indexes with nested fields are not fully supported in Azure Cosmos DB API for MongoDB. "\
+            "If you are using unique index where the nested fields are docs (not arrays), you may raise a support ticket to enable the functionality."
+        print(unique_index_nested_msg)
+    print("\n")
