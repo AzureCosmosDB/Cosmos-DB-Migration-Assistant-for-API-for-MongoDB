9e45f704e91d6dd0526838d68e98fba113619bca
Shweta Nayak <16984456+nayakshweta@users.noreply.github.com>
2021-12-05 17:20:58 +0530

    Adds rule to check whether TTL indexes are being used on fields other than _ts.

--- rules/assess_partially_supported_indexing_features.py
+++ rules/assess_partially_supported_indexing_features.py
@@ -1,8 +1,10 @@
 from .assess_unique_index import *
 from .assess_compound_index_nested_field import *
 from .assess_unique_index_nested_field import *
+from .assess_ttl_index import *
 
 def assess_partially_supported_indexing_features(self):
     assess_unique_index(self)
     assess_compound_index_nested_field(self)
     assess_unique_index_nested_field(self)
+    assess_ttl_index(self)
